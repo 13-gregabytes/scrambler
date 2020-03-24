fa0836d3c1daf513c5b071dbc1c4ab24f9df8b39
gbochan <gbochan@opentext.com>
2020-03-24 20:48:33 +0000

    ScrambleFilter -> CORSFilter

--- src/main/java/bochan/greg/main/filter/GregCORSFilter.java
+++ src/main/java/bochan/greg/main/filter/GregCORSFilter.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class GregScrambleFilter implements Filter {
+public class GregCORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+//        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
 
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
