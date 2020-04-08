64d25e4001dfd6cfd0033eb70b0efc3dbd1aede5
gbochan <gbochan@opentext.com>
2020-04-08 21:41:53 +0100

    Bit more canvas arrows

--- src/main/resources/static/scripts/oll.js
+++ src/main/resources/static/scripts/oll.js
@@ -33,7 +33,7 @@ LastLayer.go = function go() {
         ["R2", "D", "R'", "U2", "R", "D'", "R'", "U2", "R'"]
     );
 
-    $("body").append("<hr>");
+    $("body").append("<a id='pll-section'><hr></a>");
 
     LastLayer.drawPLL("Ua",
         ["B1:B", "B2:B", "B3:B", "L1:R", "L2:G", "L3:R", "R1:O", "R2:R", "R3:O", "F1:G", "F2:O", "F3:G"],
@@ -44,16 +44,33 @@ LastLayer.go = function go() {
     LastLayer.drawPLL("Ub",
         ["B1:B", "B2:B", "B3:B", "L1:R", "L2:O", "L3:R", "R1:O", "R2:G", "R3:O", "F1:G", "F2:R", "F3:G"],
         ["M2", "U'", "M", "U2", "M'", "U'", "M2"],
-        ["R2", "U", "R", "U", "R'", "U'", "R'", "U'", "R'", "U", "R'"]
+        ["R2", "U", "R", "U", "R'", "U'", "R'", "U'", "R'", "U", "R'"],
+        ["4:6", "8:4", "6:8"]
     );
     LastLayer.drawPLL("Z",
-        ["B1:B", "B2:R", "B3:B", "L1:R", "L2:B", "L3:R", "R1:O", "R2:G", "R3:O", "F1:G", "F2:O", "F3:G"],
-        ["M", "U'", "(M2", "U')2", "M", "U2", "M2"],
-        ["M2", "U", "M2", "U", "M'", "U2", "M2", "U2", "M'", "U2"]
+    ["B1:B", "B2:R", "B3:B", "L1:R", "L2:B", "L3:R", "R1:O", "R2:G", "R3:O", "F1:G", "F2:O", "F3:G"],
+    ["M", "U'", "(M2", "U')2", "M", "U2", "M2"],
+    ["M2", "U", "M2", "U", "M'", "U2", "M2", "U2", "M'", "U2"],
+    ["4:2", "8:6"]
     );
     LastLayer.drawPLL("H",
         ["B1:B", "B2:G", "B3:B", "L1:R", "L2:O", "L3:R", "R1:O", "R2:R", "R3:O", "F1:G", "F2:B", "F3:G"],
-        ["M2", "U", "M2", "U2", "M2", "U", "M2"]
+        ["M2", "U", "M2", "U2", "M2", "U", "M2"],
+            undefined,
+            ["2:8", "4:6"]
+    );
+    LastLayer.drawPLL("E",
+        ["B1:R", "B2:B", "B3:O", "L1:G", "L2:R", "L3:B", "R1:G", "R2:O", "R3:B", "F1:R", "F2:G", "F3:O"],
+        ["x'", "(R", "U'", "R'", "D)", "(R", "U", "R'", "D')", "(R", "U", "R'", "D)", "(R", "U'", "R'", "D')", "x"],
+            undefined,
+            ["1:7", "3:9"]
+    );
+
+    $("body").append("<hr>");
+
+    LastLayer.drawPLL("Edge flip",
+        ["B1:B", "B2:B", "B3:B", "L1:R", "L2:Y", "L3:R", "R1:O", "R2:Y", "R3:O", "F1:G", "F2:G", "F3:G", "U4:R", "U6:O"],
+        ["(R", "E", "R2", "E2", "R)", "U2<br>", "(R'", "E2", "R2", "E'", "R')", "U2"]
     );
 };
 
@@ -86,7 +103,7 @@ LastLayer.drawPLL = function draw(pattern, squares, solve, altsolve, moves) {
 
     cube.find(".title .text").html(pattern);
 
-    let solvetext = solve.join(" ").toUpperCase();
+    let solvetext = solve.join(" ");
     solvetext += ((altsolve != undefined) ? ("<br><br>" + altsolve.join(" ")) : "");
 
     cube.find(".solve .text").html(solvetext);
@@ -113,18 +130,21 @@ LastLayer.drawMoves = function drawMoves(moves, canvas) {
 
     ctx.clearRect(0,0,1000,1000)
 
+    let xScew = 7;
+    let yScew = 7;
+
     let coords = {
-        "1": { "x": 27,  "y": 27  },
-        "2": { "x": 83,  "y": 27  },
-        "3": { "x": 137, "y": 27  },
+        "1": { "x": 27  - xScew, "y": 27 - yScew  },
+        "2": { "x": 83  - xScew, "y": 27 - yScew  },
+        "3": { "x": 137 - xScew, "y": 27 - yScew  },
 
-        "4": { "x": 27,  "y": 83  },
-        "5": { "x": 83,  "y": 83  },
-        "6": { "x": 137, "y": 83  },
+        "4": { "x": 27  - xScew, "y": 83 - yScew  },
+        "5": { "x": 83  - xScew, "y": 83 - yScew  },
+        "6": { "x": 137 - xScew, "y": 83 - yScew  },
 
-        "7": { "x": 27,  "y": 137 },
-        "8": { "x": 83,  "y": 137 },
-        "9": { "x": 137, "y": 137 }
+        "7": { "x": 27  - xScew, "y": 137 - yScew },
+        "8": { "x": 83  - xScew, "y": 137 - yScew },
+        "9": { "x": 137 - xScew, "y": 137 - yScew }
     };
 
     for (let m in moves) {
@@ -132,33 +152,45 @@ LastLayer.drawMoves = function drawMoves(moves, canvas) {
         let start = move[0];
         let end = move[1];
 
+        let startX = coords[start].x;
+        let startY = coords[start].y;
+
+        let endX = coords[end].x;
+        let endY = coords[end].y;
+
         ctx.beginPath();
-        ctx.moveTo(coords[start].x, coords[start].y);
-        ctx.lineTo(coords[end].x, coords[end].y);
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
         ctx.lineWidth = 3;
         ctx.strokeStyle = "#0000FF";
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.arc(coords[start].x, coords[start].y, 5, 0, 2 * Math.PI);
+        ctx.arc(startX, startY, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "#00FF00";
         ctx.fill();
 
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
-        ctx.arc(coords[start].x, coords[start].y, 5, 0, 2 * Math.PI);
+        ctx.arc(startX, startY, 5, 0, 2 * Math.PI);
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.arc(coords[end].x, coords[end].y, 5, 0, 2 * Math.PI);
+        ctx.arc(endX, endY, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "#FF0000";
         ctx.fill();
 
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.strokeStyle = "#000000";
-        ctx.arc(coords[end].x, coords[end].y, 5, 0, 2 * Math.PI);
+        ctx.arc(endX, endY, 5, 0, 2 * Math.PI);
         ctx.stroke();
+
+        coords[start].x = startX + (xScew * 2);
+        coords[start].y = startY + (yScew * 2);
+
+        coords[end].x = endX + (xScew * 2);
+        coords[end].y = endY + (yScew * 2);
     }
 };
